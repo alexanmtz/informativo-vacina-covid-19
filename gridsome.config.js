e42ac1e74477a4c53724960c7b6d63d7a0497ff3
Alexandre Magno <alexanmtz@gmail.com>
2021-01-08 21:54:52 +0100

    text change

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,7 +16,7 @@ function addStyleResource (rule) {
 }
 
 module.exports = {
-  siteName: 'Perguntas e respostas sobre a vacina para combater o Covid-19',
+  siteName: 'Perguntas e respostas sobre a vacina para combater o vírus da Covid-19',
   siteUrl: 'https://vacina-covid.com.br',
   templates: {
     Doc: '/:slug',
