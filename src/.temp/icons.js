f5c9427fbfdc0f2e11e94c697d87ebbce16aea55
WManzoli <willmanzoli@gmail.com>
2021-02-17 16:56:35 -0300

    feat:'image placeholder'

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/favicon.png?width=76&key=b04ea44"
+      "src": "/assets/static/src/favicon.png?width=76&key=98bb0af"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/favicon.png?width=152&key=b04ea44"
+      "src": "/assets/static/src/favicon.png?width=152&key=98bb0af"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/favicon.png?width=120&key=b04ea44"
+      "src": "/assets/static/src/favicon.png?width=120&key=98bb0af"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/favicon.png?width=167&key=b04ea44"
+      "src": "/assets/static/src/favicon.png?width=167&key=98bb0af"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/favicon.png?width=180&key=b04ea44"
+      "src": "/assets/static/src/favicon.png?width=180&key=98bb0af"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/favicon.png?width=16&key=fded904"
+      "src": "/assets/static/src/favicon.png?width=16&key=061a117"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/favicon.png?width=32&key=fded904"
+      "src": "/assets/static/src/favicon.png?width=32&key=061a117"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/favicon.png?width=96&key=fded904"
+      "src": "/assets/static/src/favicon.png?width=96&key=061a117"
     }
   ]
 }
--- src/.temp/routes.js
+++ src/.temp/routes.js
@@ -1,6 +1,6 @@
-const c1 = () => import(/* webpackChunkName: "page--src-pages-404-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\os\\informativo-vacina-covid-19\\src\\pages\\404.vue")
-const c2 = () => import(/* webpackChunkName: "page--src-templates-doc-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\os\\informativo-vacina-covid-19\\src\\templates\\Doc.vue")
-const c3 = () => import(/* webpackChunkName: "page--src-pages-index-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\os\\informativo-vacina-covid-19\\src\\pages\\Index.vue")
+const c1 = () => import(/* webpackChunkName: "page--src-pages-404-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\informativo-vacina-covid-19\\src\\pages\\404.vue")
+const c2 = () => import(/* webpackChunkName: "page--src-templates-doc-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\informativo-vacina-covid-19\\src\\templates\\Doc.vue")
+const c3 = () => import(/* webpackChunkName: "page--src-pages-index-vue" */ "C:\\Users\\WMANZ\\Desktop\\projetos\\informativo-vacina-covid-19\\src\\pages\\Index.vue")
 
 export default [
   {
